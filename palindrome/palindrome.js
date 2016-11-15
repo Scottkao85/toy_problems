0fdd0d2cf0c951885268a3864e28af597c302849
Scott Kao <scottkao85@gmail.com>
2016-11-14 20:20:43 -0800

    add: completed recursive palindrome

--- palindrome/palindrome.js
+++ palindrome/palindrome.js
@@ -11,4 +11,17 @@ var palindromeSimple = function(str) {
   return false;
 }
 
+var palindromeRecursive = function(str) {
+  // check if str length is less than or equal to 0
+  if(str.length <= 1) {
+    return true;
+  }
+  // check if the first letter is not the same as the last letter
+  // return false if the first comparison fails
+  if(str.charAt(0) != str.charAt(str.length-1)) {
+    return false;
+  }
+  // recursively compare the rest/"middle" of the remaining str
+  return palindromeRecursive(str.substr(1, str.length-2));
+}
 
