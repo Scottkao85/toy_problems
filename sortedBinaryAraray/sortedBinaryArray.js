d96ee87b3b5f28a8e85a1817ab1dc8cde4bcbf85
scott kao <scott_kao@apple.com>
2017-05-04 19:06:15 -0700

    solution for sortedBiaryArray

--- sortedBinaryAraray/sortedBinaryArray.js
+++ sortedBinaryAraray/sortedBinaryArray.js
@@ -6,6 +6,29 @@
 // Input: { 1, 0, 1, 0, 1, 0, 0, 1 }
 // Output: { 0, 0, 0, 0, 1, 1, 1, 1 }
 
+var array = [1, 0, 1, 0, 1, 0, 0, 1]
+
 var sortBinaryArray = function(array) {
-	
+	var i = 0;
+	var j = array.length - 1;
+
+	// as long as we're not the same
+	while(i < j) {
+		// if the left item is greater then it must be 1
+		// so you need to swap them
+		if (arr[i] > arr[j]) {
+			[arr[i], arr[j]] = [arr[j], arr[i]];
+	    }
+
+	    // if the current item is zero increment one to find the next
+	    if (arr[i] === 0) {
+	      i = i + 1;
+	    }
+
+	    // if the current item is one then decrease by one to find the next
+	    if (arr[j] === 1) {
+	      j = j - 1;
+	    }
+	}
+	return array
 };
