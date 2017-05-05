83fdec743946f55927e0da9d65efd3dfb6e2c8a6
scott kao <scott_kao@apple.com>
2017-05-04 18:55:16 -0700

    started problem sortedBinaryArray

---
+++ sortedBinaryAraray/sortedBinaryArray.js
@@ -0,0 +1,11 @@
+// Sort binary array in linear time
+// Given an binary array, sort it in linear time and constant space. Output should print contain all zeroes followed by all ones.
+
+// For example,
+
+// Input: { 1, 0, 1, 0, 1, 0, 0, 1 }
+// Output: { 0, 0, 0, 0, 1, 1, 1, 1 }
+
+var sortBinaryArray = function(array) {
+	
+};
