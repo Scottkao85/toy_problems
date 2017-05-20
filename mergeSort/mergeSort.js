1aaa94a0763f038c1dd8fa811c6a1bbe26ccfae0
Scott Kao <scottkao85@gmail.com>
2017-05-20 10:10:42 -0700

    progress in mergeSort

--- mergeSort/mergeSort.js
+++ mergeSort/mergeSort.js
@@ -1,37 +1,14 @@
 var mergeSort = function(arr) {
 
-  // if not array or just 1 item.
-  if(arr.length < 2 || !Array.isArray.(arr)) {
+  if (arr.length) {
+    
     return arr;
   }
 
-  var mid = Math.floor(arr.length/2);
-  var left = arr.slice(0, mid);
-  var right = arr.slice(mid, arr.length);
-
-  var compare = function(a, b) {
-    return a > b ? 1 : -1;
-  };
-
-  var merge = function(left, right, result) {
-    var result = [];
-    
-    while left(left.length && right.length) {
-      if(left[0] <= r[0]) {
-        result.push(left.shift());
-      } else {
-        result.push(right.shift());
-      }
-    }
-
-    while(left.length) {
-      result.push(left.shift());
-    }
-
-    while(right.length) {
-      result.push(right.shift());
-
-    }
-    return result;
+  var splitArr = function(arr) {
+    var arrA = [];
+    var arrB = [];
+  
   }
+
 }
